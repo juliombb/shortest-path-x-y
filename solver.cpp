441d16e40205725ca028d79d6487a41808c6e1fc
julio-barros-movile <julio.barros@movile.com>
2019-06-23 22:13:17 +0000

    pointerless

--- solver.cpp
+++ solver.cpp
@@ -10,17 +10,17 @@ bool visited(int point, unsigned long long int mask);
 
 unsigned long long int visit(unsigned long long int mask, int point);
 
-vector<int>*** matrix;
+vector<vector<vector<int>>> matrix;
 // bi dimensional -> x = ponto atual, y = mascara
 
 void initMatrix(int n) {
-    matrix = (vector<int>***) malloc(n*sizeof(vector<int>**));
+    matrix = vector<vector<vector<int>>>(n);
 
     for (int i = 0; i < n; ++i) {
         unsigned long long possible_masks = (1<<n);
-        matrix[i] = (vector<int>**) malloc(possible_masks*sizeof(vector<int>*));
+        matrix[i] = vector<vector<int>>(possible_masks);
         for (int j = 0; j < possible_masks; ++j) {
-            matrix[i][j] = nullptr;
+            matrix[i][j] = vector<int>(0);
         }
     }
 }
@@ -32,19 +32,10 @@ double getDistance(Point p1, Point p2){
 	return sqrt(delta_x * delta_x + delta_y * delta_y); 
 }
 
-double getFullCost(vector<Point> points) {
-    double sum = 0;
-    for (int i = 1; i < points.size(); ++i) {
-        sum += getDistance(points[i], points[i-1]);
-    }
-
-    return sum;
-}
-
 double getFullCost(vector<int> points, Instance &instance) {
     double sum = 0;
     for (int i = 1; i < points.size(); ++i) {
-        sum += getDistance(instance.points[points[i]], instance.points[points[i-1]]);
+        sum += getDistance(instance.points[points.at(i)], instance.points[points.at(i-1)]);
     }
 
     return sum;
@@ -117,7 +108,7 @@ unsigned long long visit(unsigned long long mask, int point) {
 }
 
 
-vector<int>* solveTopDownWithMask(
+vector<int> solveTopDownWithMask(
         Instance &instance, unsigned long long mask, int current_pt,
         int timelimit, chrono::high_resolution_clock::time_point &started
         ) {
@@ -129,19 +120,19 @@ vector<int>* solveTopDownWithMask(
 //        return vector<int>(0);
 //    }
 
-    if (matrix[current_pt][mask] != nullptr) {
+    if (!matrix[current_pt][mask].empty()) {
         return matrix[current_pt][mask];
     }
 
     if (allVisited(mask, instance.n - 1)) {
-        vector<int>* res = (vector<int>*) malloc(sizeof(vector<int>));
-        res->push_back(instance.n - 1);
-        res->push_back(current_pt);
+        vector<int> res;
+        res.push_back(instance.n - 1);
+        res.push_back(current_pt);
         matrix[current_pt][mask] = res;
         return res;
     }
 
-    vector<int>* best_sol = (vector<int>*) malloc(sizeof(vector<int>));
+    vector<int> best_sol;
     double best_sol_cost = -1;
 
     for (int point = 1; point < instance.n - 1; ++point) {
@@ -150,16 +141,13 @@ vector<int>* solveTopDownWithMask(
         }
 
         unsigned long long newMask = visit(mask, point);
-        vector<int>* current_sol = (vector<int>*) malloc(sizeof(vector<int>));
-        (*current_sol) = *solveTopDownWithMask(instance, newMask, point, timelimit, started);
-        current_sol->push_back(current_pt);
-        double current_sol_cost = getFullCost((*current_sol), instance);
+        vector<int> current_sol = solveTopDownWithMask(instance, newMask, point, timelimit, started);
+        current_sol.push_back(current_pt);
+        double current_sol_cost = getFullCost(current_sol, instance);
 
         if (best_sol_cost == -1 || best_sol_cost > current_sol_cost) {
             best_sol_cost = current_sol_cost;
             best_sol = current_sol;
-        } else {
-            delete current_sol;
         }
     }
 
@@ -172,7 +160,7 @@ vector<int> solveTopDown(Instance &instance, int timelimit, chrono::high_resolut
     // mask = mascara com n bits. se o bit i está ocupado o ponto i ja foi visitado na recursão em questão
 
     initMatrix(instance.n - 1);
-    vector<int> res = *solveTopDownWithMask(instance, mask, 0, timelimit, started);
+    vector<int> res = solveTopDownWithMask(instance, mask, 0, timelimit, started);
 
     std::reverse(res.begin(), res.end()); // nossa funcao retorna com n na primeira posicao
 
